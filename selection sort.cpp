39c43d2c17a6b9a8b92f13f109c0994f870ecf1c
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2020-12-12 11:04:44 +0530

    Update selection sort.cpp

--- selection sort.cpp
+++ selection sort.cpp
@@ -1,26 +1,35 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-	int a[100]={10,33,27,14,35,19,42,44},i,j,temp;
-	for(i=0;i<8;i++)
+	int n;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	cin>>a[i];
+	
+	for(int i=0;i<n;i++)
 	{
-		cout<<"\n";
-		for(j=i+1;j<8;j++)
+		int j;
+		int k=0;
+		int temp=i;
+		for(j=i+1;j<n;j++)
 		{
-			if(a[i]>a[j])
+			if(a[j]<a[temp])
 			{
-				temp=a[i];
-				a[i]=a[j];
-				a[j]=temp;
-			
+				temp=j;
 			}
 		}
-		for(int k=0;k<8;k++)
-		cout<<a[k]<<"\t";
+		swap(a[i],a[temp]);
+		cout<<"\n\n";
+		for(int k=0;k<n;k++)
+		{
+			cout<<a[k]<<"\t";
+		}
 	}
-	cout<<"\nfinal result"<<endl;
-	for(i=0;i<8;i++)
-		cout<<a[i]<<"\t";
 	
+	cout<<"\n\n";
+for(int i=0;i<n;i++)
+	cout<<a[i]<<"\t";
 }
