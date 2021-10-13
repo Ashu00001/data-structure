e4870d24416b672773b106caea8bbd5f31cd1760
Ashutosh Gupta <43481150+Ashu00001@users.noreply.github.com>
2021-10-14 00:28:51 +0530

    Create level order traversal.cpp

---
+++ level order traversal.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+  int key;
+  Node *left,*right;
+  Node(int data)
+  {
+      key=data;
+      right=NULL;
+      left=NULL;
+  }
+};
+
+void printLevelOrder(Node *root)
+{
+    queue<Node *>q ;
+    q.push(root);
+    while(!q.empty())
+    {
+        Node * curr=q.front();
+        q.pop();
+        cout<<curr->key<<" ";
+        if(curr->left!=NULL)
+        {
+            q.push(curr->left);
+        }
+        if(curr->right!=NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+    
+}
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+ 
+    cout << "Level Order traversal of binary tree is \n";
+    printLevelOrder(root);
+ 
+}
